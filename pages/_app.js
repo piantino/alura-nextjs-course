c121ae7cc856993f34171c3fa5f57de91b3d60e7
Andre Piantino <andre.piantino@tjsc.jus.br>
2023-01-23 17:54:20 -0300

    Added Font and page title

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import GlobalStyle from '../src/theme/GlobalStyle'
+import { Open_Sans } from '@next/font/google'
+import PageTitle from '../src/components/PageTitle'
+
+const font = Open_Sans({ subsets: ['latin'] })
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
-            <Component {...pageProps} />
+            <PageTitle>Alura Cases</PageTitle>
+            <main className={font.className}>
+                <Component {...pageProps} />
+            </main>
             <GlobalStyle></GlobalStyle>
         </>
     )
