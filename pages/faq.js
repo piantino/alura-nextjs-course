05fcad7efd72cb196f444c393e9f55ad497ec5fe
Andre Piantino <andre.piantino@tjsc.jus.br>
2023-01-23 12:43:03 -0300

    Project created

---
+++ pages/faq.js
@@ -0,0 +1,8 @@
+export default function FaqPage() {
+    return (
+        <div>
+            <h1>Alura Cases - Páginas e perguntas FAQ</h1>
+            <a href="/">Ir para FAQ</a>
+        </div>
+    )
+}
