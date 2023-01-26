c760072516ad00eec9dd0c0689355296de37d13a
Andre Piantino <andre.piantino@tjsc.jus.br>
2023-01-26 14:41:22 -0300

    Added a API

--- pages/private/index.js
+++ pages/private/index.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import nookies from 'nookies';
+import { useEffect, useState } from "react";
 import Link from "../../src/components/Link";
 
 const PASSWORD = '123456'
@@ -26,6 +27,7 @@ export async function getServerSideProps(context) {
 export default function PrivatePage(props) {
 
     const router = useRouter();
+    const [user, setUser] = useState([]);
 
     function logout() {
         nookies.destroy(null, 'PASSWORD', {
@@ -34,9 +36,25 @@ export default function PrivatePage(props) {
         router.push('/private')
     }
 
+    useEffect(() => {
+        fetch("/api/me/")
+          .then(res => res.json())
+          .then(
+            (result) => {
+                setUser(result)
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              console.error(error)
+            }
+          )
+      }, [])
+
     return (
         <>
-            <h1>Logado</h1>
+            <h1>Logado {user.name}</h1>
             <Link href="/">Ir para a home</Link>
             <button onClick={logout}>Sair</button>
         </>
